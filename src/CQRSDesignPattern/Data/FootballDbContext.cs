34933a7b069f9108e69c60e5f84f32b67715d872
dev7k <dev7k@protonmail.com>
2021-08-21 19:32:43 +0200

    Added FootballDbContext

---
+++ src/CQRSDesignPattern/Data/FootballDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using CQRSDesignPattern.Models;
+
+#nullable disable
+
+namespace CQRSDesignPattern.Data
+{
+    public partial class FootballDbContext : DbContext
+    {
+        public FootballDbContext()
+        {
+        }
+
+        public FootballDbContext(DbContextOptions<FootballDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- src/CQRSDesignPattern/Startup.cs
+++ src/CQRSDesignPattern/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CQRSDesignPattern.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CQRSDesignPattern
 {
@@ -24,6 +26,8 @@ namespace CQRSDesignPattern
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<FootballDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
