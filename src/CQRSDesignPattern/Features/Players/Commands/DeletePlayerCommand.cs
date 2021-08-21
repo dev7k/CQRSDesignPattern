a76383c1835a27eb3e30fbc105a39ff3dcea2c40
dev7k <dev7k@protonmail.com>
2021-08-21 20:55:46 +0200

    Implemented Commands using CQRS Pattern

---
+++ src/CQRSDesignPattern/Features/Players/Commands/DeletePlayerCommand.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using CQRSDesignPattern.Services;
+using MediatR;
+
+namespace CQRSDesignPattern.Features.Players.Commands
+{
+    public class DeletePlayerCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public int? ShirtNo { get; set; }
+        public string Name { get; set; }
+        public int? Appearances { get; set; }
+        public int? Goals { get; set; }
+
+        public class DeletePlayerCommandHandler : IRequestHandler<DeletePlayerCommand, int>
+        {
+            private readonly IPlayersService _playerService;
+
+            public DeletePlayerCommandHandler(IPlayersService playerService)
+            {
+                _playerService = playerService;
+            }
+
+            public async Task<int> Handle(DeletePlayerCommand command, CancellationToken cancellationToken)
+            {
+                var player = await _playerService.GetPlayerById(command.Id);
+                if (player == null)
+                    return default;
+
+                return await _playerService.DeletePlayer(player);
+            }
+        }
+    }
+}
